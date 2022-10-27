8152e1e009e5493a958bc72f912efb280f9aae48
tai-martins <tainara@gec.inatel.br>
2022-10-27 09:35:13 -0300

    docker configuration by dockerfile and docker-compose files

--- quotation-management/src/main/java/br/inatel/idp/quotationmanagement/controller/StockController.java
+++ quotation-management/src/main/java/br/inatel/idp/quotationmanagement/controller/StockController.java
@@ -1,5 +1,19 @@
 package br.inatel.idp.quotationmanagement.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.inatel.idp.quotationmanagement.service.StockService;
+
+/**
+ * @author Tainara Martins.
+ * @since October 2022
+ */
+
+@RestController
+@RequestMapping("/stock")
 public class StockController {
+	
+	private StockService service;
 
 }
